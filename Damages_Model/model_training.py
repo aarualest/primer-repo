13b20b665075d42f39b2718919414a0d27691ce5
Anderson Ruales <aarualest@unal.edu.co>
2024-01-29 18:43:50 -0500

    Script entrenamiento

---
+++ Damages_Model/model_training.py
@@ -0,0 +1,8 @@
+import time
+
+# Lista de etapas en el entrenamiento de un modelo
+etapas = ["Inicializando el modelo", "Cargando datos", "Entrenando el modelo", "Evaluando el modelo", "Entrenamiento completado"]
+
+for etapa in etapas:
+    print(etapa)
+    time.sleep(3)
